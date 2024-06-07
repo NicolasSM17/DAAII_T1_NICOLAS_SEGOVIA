43a7c6f05197f886a4c37a90163dfa3142d04798
NicolasSM17 <segoviamatosjesunicolas@gmail.com>
2024-06-07 17:00:27 -0500

    inicio PREGUNTA 1

---
+++ src/main/java/pe/edu/cibertec/DAAII_T1_Nicolas_Segovia/DaaiiT1NicolasSegoviaApplication.java
@@ -0,0 +1,13 @@
+package pe.edu.cibertec.DAAII_T1_Nicolas_Segovia;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DaaiiT1NicolasSegoviaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DaaiiT1NicolasSegoviaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pe/edu/cibertec/DAAII_T1_Nicolas_Segovia/DaaiiT1NicolasSegoviaApplicationTests.java
@@ -0,0 +1,13 @@
+package pe.edu.cibertec.DAAII_T1_Nicolas_Segovia;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DaaiiT1NicolasSegoviaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
