18c203f9ab59068c4783912a4a751caea4afe846
NicolasSM17 <segoviamatosjesunicolas@gmail.com>
2024-06-07 17:22:49 -0500

    fin PREGUNTA 1

---
+++ src/main/java/pe/edu/cibertec/DAAII_T1_Nicolas_Segovia/model/bd/Rol.java
@@ -0,0 +1,19 @@
+package pe.edu.cibertec.DAAII_T1_Nicolas_Segovia.model.bd;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+@Entity
+@Table(name = "rol")
+public class Rol {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer idrol;
+
+    private String nomrol;
+}
